33c5125f6b59ebb64d63453ca0bad0203e2b0e1f
i1ikey0u <syncvenus@gmail.com>
2017-08-28 16:34:33 +0800

    更上上个版本的提交错误

--- watchdog_of_wxgzh.py
+++ watchdog_of_wxgzh.py
@@ -23,6 +23,11 @@ def get_gzh_lists(keys, gzh_num=1):
 	"""
 	根据关键字keys，搜索公众号，未登录搜狗情况下，最多搜索10页，共计100个公众号。
 	gzh_num为从搜索结果中提取多少个公众号，数组gzh_list，内容为公众号列表
+	"""
+
+	gzh_lists = []
+	m = 1
+	while gzh_num > 0:
 		pages_url = "http://weixin.sogou.com/weixin?query=" + parse.quote(keys) + "&type=1&page=%s" % m
 		print("当前搜索URL为：", pages_url)
 		cots = request.urlopen(pages_url).read().decode('utf-8')
@@ -34,11 +39,16 @@ def get_gzh_lists(keys, gzh_num=1):
 			gzhbox2 = gzhlist_soup.find_all('div', class_="gzh-box2")
 			if len(gzhbox2) < gzh_num:
 				num = len(gzhbox2)
+			elif len(gzhbox2) > gzh_num:
+				num = gzh_num
 			print("从当前页获取 %i 个公众号。" % num)
 			for n in range(0, num):
+				uni_gzh = gzhbox2[n].find('a').attrs['href']
 				gzh_lists.append(uni_gzh)
 			gzh_num = gzh_num - num
+			m += 1
 	return gzh_lists
+
 
 
 def get_gzh_content_top10(gzh_url):
@@ -72,6 +82,7 @@ def grep_gzh(url, key):
 	'''
 	对给定文章中的关键字进行过滤，输出含有指定关键字的文章链接
 	'''
+	contents = request.urlopen(url).read().decode('utf-8')
 	pat = re.compile(key)
 	res = len(pat.findall(contents))
 	if res > 0:
@@ -82,6 +93,8 @@ def grep_gzh(url, key):
 
 if __name__ == "__main__":
 	print("f*ck wechat !")
+	version = "version v3.2 by shuichon @ 2017年8月28日"
+	if len(sys.argv) > 1:
 		if sys.argv[1].startswith('-'):
 			option = sys.argv[1][1:]
 			if option == 'v':
@@ -104,33 +117,41 @@ if __name__ == "__main__":
 				print(version)
 			elif option == 'gzh':
 				print('当前参数数量为：', len(sys.argv[:]), sys.argv[:])
+				print('搜索和"%s"相关的公众号' % sys.argv[2])
 				if (len(sys.argv[:]) > 3) and (sys.argv[3][1:] == 'gnum'):
 					option2 = sys.argv[3][1:]
 					print('获取%i个微信公众号进行内容识别' % int(sys.argv[4]))
 					gzh_url_l = get_gzh_lists(keys=sys.argv[2], gzh_num=int(sys.argv[4]))
 					for l in gzh_url_l:
 						print("当前公众号访问URL:")
+						print(l)
 						print("当前公众号最近10篇公众号文章URL:")
 						for wz in get_gzh_content_top10(l):
 							print(wz)
+				elif (len(sys.argv[:]) > 3) and (sys.argv[3][1:] == 'key'):
 					option2 = sys.argv[3][1:]
 					print('使用 "%s" 关键字搜索微信公众号' % sys.argv[2])
 					gzh_url_l = get_gzh_lists(keys=sys.argv[2])
 					for l in gzh_url_l:
 						print(l)
+						for wz in get_gzh_content_top10(l):
 							grep_gzh(wz, key=sys.argv[4])
 				else:
 					print(get_gzh_lists(keys=sys.argv[2]))
+			elif option == 'kws':
+				option2 = sys.argv[2]
 				print('公众号内容识别关键字为：' + option2)
 				if len(sys.argv[:]) == 3:
 					print('默认获取1个微信公众号')
 					gzh_url_l = get_gzh_lists(keys=sys.argv[2])
 					print(gzh_url_l)
 				elif (len(sys.argv[:]) > 3) and (sys.argv[3][1:] == 'gnum') :
+					option3 = sys.argv[3]
 					print('获取%i个微信公众号' % int(sys.argv[4]))
 					gzh_url_l = get_gzh_lists(keys=sys.argv[2], gzh_num=int(sys.argv[4]))
 					print(gzh_url_l)
 				for l in gzh_url_l:
+					print("当前公众号最近10篇公众号文章URL:")
 					for wz in get_gzh_content_top10(l):
 						print(wz)
 			else:
